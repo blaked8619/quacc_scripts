19b703d517d39753d92b067142b3421283ace72f
Blake Dallmann <bd8619@princeton.edu>
2025-12-18 14:18:25 -0500

    Update jobs.py

--- src/quacc_scripts/mlp/jobs.py
+++ src/quacc_scripts/mlp/jobs.py
@@ -13,6 +13,11 @@ from ase.filters import FrechetCellFilter
 from ase.optimize import BFGS
 from fairchem.core import pretrained_mlip, FAIRChemCalculator
 
+from ase.thermochemistry import IdealGasThermo
+from ase.vibrations import Vibrations
+from ase import units
+from emmet.core.symmetry import PointGroupData
+
 
 @job
 def relax_mof(atoms, model_path):
@@ -115,4 +120,92 @@ def phonon_mp(atoms):
     np.savetxt("thermal_properties.txt", data, fmt="%12.3f %15.7f %15.7f %15.7f", header=header)
 
     return {"thermal_properties": data}
+
+@job
+def relax_gas(atoms):
+    try:
+        magmoms = atoms.get_magnetic_moments()
+        total_magmom = np.sum(magmoms)
+
+        # Spin multiplicity = |total magnetization| + 1
+        spin_multiplicity = int(round(abs(total_magmom))) + 1
+
+        # Store it
+        atoms.info['spin'] = spin_multiplicity
+
+        print(f"Total magnetization: {total_magmom:.2f}")
+        print(f"Inferred spin multiplicity: {spin_multiplicity}")
+    except:
+        print("No magnetic moments found (non-spin-polarized calculation)")
+        atoms.info['spin'] = 1  # Default to singlet
+
+    model_name = "uma-s-1p1"
+    predictor = pretrained_mlip.get_predict_unit(model_name, device="cuda")
+    calc = FAIRChemCalculator(predictor, task_name="omol")
+
+    runner = RelaxCalc(calculator = calc, optimizer = BFGS, max_steps = 100000, traj_file = "relax.traj", fmax=1e-3, relax_atoms = True, relax_cell = True)
+    result = runner.calc(atoms)
+    mlip_energy = atoms.get_potential_energy()
+    
+    return {"output_atoms": atoms, "mlip_energy": mlip_energy}
+
+@job
+def gas_vibrations(atoms, mlip_energy):
+    vib = Vibrations(atoms)
+    vib.run()
+    vib_energies = vib.get_energies()
+
+    #find the symmetry number
+    mol = AseAtomsAdaptor().get_molecule(atoms, charge_spin_check=False)
+    point_group_data = PointGroupData().from_molecule(mol)
+    sigma = point_group_data.rotation_number or 1
+    sym = sigma
+
+    #find the geometry
+    natoms = len(atoms)
+    if natoms == 1:
+        geometry = "monatomic"
+    elif point_group_data.linear:
+        geometry = "linear"
+    else:
+        geometry = "nonlinear"
+
+    #find the spin quantum number
+    spin = (atoms.info['spin'] - 1)/2 # need to change spin_multiplcity from atoms.info to a total spin
+    
+    igt = IdealGasThermo(vib_energies, geometry, potentialenergy=mlip_energy, atoms=atoms, symmetrynumber=sym, spin=spin)
+
+    #find the corrections
+    temperatures = np.arange(0, 1001, 1)  # 0 to 1000 K, step of 1 K
+    G_free_energy = []
+    enthalpy = []
+    entropy = []
+
+    pressure = 100000  # Pa (1bar), 0 atm would diverge to infinity for Gibbs
+
+    kB = units.kB #boltzmann constant
+
+    for T in temperatures:
+        G = igt.get_gibbs_energy(T, pressure)
+        H = igt.get_enthalpy(T)
+        S = igt.get_entropy(T, pressure)
+
+        G_free_energy.append(G)
+        enthalpy.append(H)
+        entropy.append(S)
+    
+    temperatures = np.array(temperatures)
+    G_free_energy = np.array(G_free_energy)
+    enthalpy = np.array(enthalpy)
+    entropy = np.array(entropy)
+
+    data = np.column_stack((temperatures, G_free_energy, enthalpy, entropy))
+
+    header_full = "T (K)          Gibbs         Enthalpy         Entropy"
+    np.savetxt("thermal_properties_full.txt", data,
+           fmt="%12.3f %15.7f %15.7f %15.7f",
+           header=header_full)
+
+    
+    return {"output_atoms": atoms, "vibration_energies": vib_energies, "spin_quantum_number": spin, "geometry": geometry, "thermal_properties": data}
 
