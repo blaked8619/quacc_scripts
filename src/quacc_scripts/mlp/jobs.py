1e3b473a4ca942e6ff092802aee6a9241e33d09a
Blake Dallmann <bd8619@princeton.edu>
2026-03-10 18:03:45 -0400

    Update jobs.py

--- src/quacc_scripts/mlp/jobs.py
+++ src/quacc_scripts/mlp/jobs.py
@@ -50,40 +50,6 @@ def relax_mof(atoms, model_path, fmax):
 
     return {"output_atoms": atoms, "energy": energy}
 
-@job
-def phonon_mof(atoms, model_path):
-    
-    calc = MACECalculator(
-        model_paths=[model_path],
-        device="cuda",
-        default_dtype="float64",
-        head="pbe_d3"
-        )
-    supercell_matrix = np.diag(
-    np.round(np.ceil(20.0 / atoms.cell.lengths()))
-    )
-    
-    phonon_calc = PhononCalc(calc, supercell_matrix = supercell_matrix).calc(atoms)
-
-    phonon = phonon_calc["phonon"]
-
-    phonon.run_mesh([20, 20, 20])
-    phonon.run_thermal_properties(t_step=1,
-                              t_max=1000,
-                              t_min=0)
-    tp_dict = phonon.get_thermal_properties_dict()
-    temperatures   = np.array(tp_dict['temperatures'])
-    free_energy    = np.array(tp_dict['free_energy'])
-    entropy        = np.array(tp_dict['entropy'])
-    heat_capacity  = np.array(tp_dict['heat_capacity'])
-
-    data = np.column_stack((temperatures, free_energy, entropy, heat_capacity))
-
-    # Define a header and save to a text file
-    header = "T (K)          Free_energy          Entropy          Heat_capacity"
-    np.savetxt("thermal_properties.txt", data, fmt="%12.3f %15.7f %15.7f %15.7f", header=header)
-
-    return {"thermal_properties": data}
 
 @job
 def QHA_mof(atoms, model_path, fmax):
@@ -91,7 +57,7 @@ def QHA_mof(atoms, model_path, fmax):
     calc = MACECalculator(
         model_paths=[model_path],
         device="cuda",
-        default_dtype="float64"
+        default_dtype="float64",
         head="pbe_d3"
         )
 
