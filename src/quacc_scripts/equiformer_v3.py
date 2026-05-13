2b4da5407d15b6edfd3d33eb36ac9c3c68a1f01a
Blake Dallmann <bd8619@princeton.edu>
2026-05-13 15:48:09 -0400

    Update equiformer_v3.py

--- src/quacc_scripts/equiformer_v3.py
+++ src/quacc_scripts/equiformer_v3.py
@@ -1,1 +1,45 @@
-fsf
+import torch
+import torch.serialization
+torch.serialization.add_safe_globals([slice])
+
+from ase.io import read, write
+from ase.visualize import view
+from ase.optimize import BFGS
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.md.velocitydistribution import Stationary
+from ase.md.langevin import Langevin
+from ase.md.nose_hoover_chain import MTKNPT
+from ase.units import fs, kB
+from ase.constraints import FixCom
+import time
+from ase.io import Trajectory
+import numpy as np
+import sys
+
+from pymatgen.io.ase import AseAtomsAdaptor
+from jobflow import Flow, job
+from jobflow.core.job import Job
+from jobflow_remote import submit_flow, set_run_config
+from monty.serialization import loadfn, dumpfn
+
+import json
+from monty.json import MontyEncoder
+from ase.filters import FrechetCellFilter
+
+@job
+def single_point(structure, checkpoint_path, taskname):
+  from fairchem.core.common.relaxation.ase_utils import OCPCalculator
+  atoms = AseAtomsAdaptor().get_atoms(structure)  # convert back inside job
+
+
+  predictor = load_predict_unit(checkpoint_path+"inference_ckpt.pt")
+  calc = FAIRChemCalculator(predictor, task_name=taskname)
+  atoms.calc = calc
+
+  energy = atoms.get_potential_energy()
+  forces = atoms.get_forces()
+  stress = atoms.get_stress(voigt=True)
+
+  max_force = np.max(np.linalg.norm(forces, axis = 1))
+  
+  return {"energy": float(energy), "forces": forces.tolist(), "stress": stress.tolist(), "max_force": float(max_force)}
