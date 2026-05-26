c02bf18904f7ecbbb3f2d8de6f874ef5cafa7c0b
Blake Dallmann <bd8619@princeton.edu>
2026-05-26 14:50:33 -0400

    Update jobs.py

--- src/quacc_scripts/mlp/jobs.py
+++ src/quacc_scripts/mlp/jobs.py
@@ -35,6 +35,31 @@ def choose_calc(calc_name):
         ocp    = OCPCalculator(checkpoint_path=checkpoint_path + "inference_ckpt.pt", cpu=False)
         pbe_d3 = TorchDFTD3Calculator(device="cuda", xc="pbe", damping="bj")
         calc   = SumCalculator([ocp, pbe_d3])
+    elif calc_name == "vasp":
+        from ase.calculators.vasp import Vasp
+        calc = Vasp(
+        # Critical parameters
+        xc="PBE",  # exchange-correlation functional ("level of theory"); sets GGA = PE here
+        encut=520,  # plane-wave kinetic energy cutoff (convergence parameter)
+        kspacing=0.4,  # k-point density (convergence parameter)
+        ivdw=12,  # van der Waals corrections (0 = none, 12 = D3(BJ))
+        # Important details
+        isif=3,  # relaxation degrees of freedom (3 = relax positions+cell shape+cell volume)
+        ediffg=-1e-4,  # relaxation convergence criterion (eV/A)
+        nsw=500,  # maximum number of relaxation steps
+        # Additional parameters
+        prec="accurate",  # high-precision calculations
+        ediff=1e-5,  # SCF convergence criterion (eV)
+        algo="fast",  # SCF convergence algorithm
+        ibrion=2,  # relaxation algorithm (2 = conjugate-gradient)
+        ismear=0,  # smearing method (0 = Gaussian smearing)
+        sigma=0.05,  # smearing width (eV)
+        lorbit=11,  # print out magnetic moments
+        lasph=True,  # non-spherical corrections for 3d elements
+        efermi="midgap",  # recommended by VASP
+        gga_compat=False,  # recommended by VASP
+        )
+
     return calc
 
 @job
