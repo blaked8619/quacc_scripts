e95ec95624df4d48ebaaacf79631dc6ab2190c2d
Blake Dallmann <bd8619@princeton.edu>
2026-06-01 13:00:22 -0400

    Update jobs.py

--- src/quacc_scripts/mlp/jobs.py
+++ src/quacc_scripts/mlp/jobs.py
@@ -35,32 +35,15 @@ def choose_calc(calc_name, atoms):
     if calc_name == "vasp":
         import os
         os.environ["ASE_VASP_COMMAND"] = "srun vasp_std"
-        os.environ["VASP_PP_PATH"] = "/home/ROSENGROUP/software/vasp/vasp_potcars/"
-        from quacc.calculators.vasp import Vasp
         
-        calc = Vasp(
-        # Critical parameters
-        input_atoms= atoms,
-        xc="PBE",  # exchange-correlation functional ("level of theory"); sets GGA = PE here
-        encut=520,  # plane-wave kinetic energy cutoff (convergence parameter)
-        kspacing=0.4,  # k-point density (convergence parameter)
-        ivdw=12,  # van der Waals corrections (0 = none, 12 = D3(BJ))
-        # Important details
-        isif=3,  # relaxation degrees of freedom (3 = relax positions+cell shape+cell volume)
-        ediffg=-1e-4,  # relaxation convergence criterion (eV/A)
-        nsw=0,  # maximum number of relaxation steps
-        # Additional parameters
-        prec="accurate",  # high-precision calculations
-        ediff=1e-5,  # SCF convergence criterion (eV)
-        algo="fast",  # SCF convergence algorithm
-        ibrion=-1,  # relaxation algorithm (2 = conjugate-gradient)
-        ismear=0,  # smearing method (0 = Gaussian smearing)
-        sigma=0.05,  # smearing width (eV)
-        lorbit=11,  # print out magnetic moments
-        lasph=True,  # non-spherical corrections for 3d elements
-        efermi="midgap",  # recommended by VASP
-        gga_compat=False,  # recommended by VASP
-        )
+        from quacc.calculators.vasp import Vasp
+        from fairchem.data.omat.vasp.sets import OMat24StaticSet
+
+        calc_defaults = MPtoASEConverter(atoms=atoms).convert_input_set(OMat24StaticSet())
+        calc_defaults |= {"pp_version": "54", "incar_copilot": "light"}
+        
+        
+        calc = Vasp(calc_defaults)
 
     elif calc_name == "UMA_OMAT":
         from fairchem.core import pretrained_mlip, FAIRChemCalculator
